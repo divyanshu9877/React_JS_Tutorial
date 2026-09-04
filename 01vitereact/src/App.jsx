08686004f3b130446f6c5f0b0d72a6d87905b201
Divyanshu Kumar <divyanshukumar9877@gmail.com>
2026-09-04 12:59:10 +0530

    understand the react and structure

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,13 @@
-
+import Chai from './chai'
 
 function App() {
 
   return (
-  <h1>Chai aur react with vite | Divyanshu</h1>
+    <>
+  <Chai/>
+  <h1>chai aur react</h1>
+  <p>test para</p>
+  </>
   )
 }
 
